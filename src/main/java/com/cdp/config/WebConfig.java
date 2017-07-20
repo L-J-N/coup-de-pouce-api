bc6c2b81facaad38a74663b3fc4d99f7ec96e5ec
jabuf <jason.buffet@gmail.com>
2017-07-20 18:16:07 +0200

    Changement sur le fichier de configuration

--- src/main/java/com/cdp/config/WebConfig.java
+++ src/main/java/com/cdp/config/WebConfig.java
@@ -3,17 +3,18 @@ package com.cdp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * Created by jabuf on 20/07/17.
  */
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**");
     }
 }
+
